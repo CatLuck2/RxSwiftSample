e163054166bea40c7d76d2da5f7586167c5b2f49
猫吉 <freeaddress834@gmail.com>
2020-12-12 13:35:10 +0900

    rootViewControllerをViewControllerに指定、残りのコードを書いた

--- RxSwiftSample/CounterRxViewModel.swift
+++ RxSwiftSample/CounterRxViewModel.swift
@@ -30,7 +30,50 @@ class CounterRxViewModel: CounterViewModelType {
     private let initialCount = 0
     private let disposeBag = DisposeBag()
 
-    func setup(input: CounterViewModelInput) {
+    init() {
+        self.outputs = self
+        resetCount()
+    }
 
+    func setup(input: CounterViewModelInput) {
+        input.countUpButton
+            .subscribe(onNext: { [weak self] in
+                self!.incrementCount()
+            })
+            .disposed(by: disposeBag)
+
+        input.countDownButton
+            .subscribe(onNext: { [weak self] in
+                self!.decrementCount()
+            })
+            .disposed(by: disposeBag)
+
+        input.countResetButton
+            .subscribe(onNext: { [weak self] in
+                self!.resetCount()
+            })
+            .disposed(by: disposeBag)
+    }
+
+    private func incrementCount() {
+        let count = countRelay.value + 1
+        countRelay.accept(count)
+    }
+
+    private func decrementCount() {
+        let count = countRelay.value - 1
+        countRelay.accept(count)
+    }
+
+    private func resetCount() {
+        countRelay.accept(initialCount)
+    }
+}
+
+extension CounterRxViewModel: CounterViewModelOutput {
+    var counterText: Driver<String?> {
+        return countRelay
+            .map {"Rxパターン:\($0)"}
+            .asDriver(onErrorJustReturn: nil)
     }
 }
