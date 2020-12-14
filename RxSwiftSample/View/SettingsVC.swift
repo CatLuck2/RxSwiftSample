bccf6b7b42d4c746657ebbac5d24941aaad08d5f
猫吉 <freeaddress834@gmail.com>
2020-12-14 23:54:38 +0900

    Sample3のコードを全部削除

--- RxSwiftSample/View/SettingsVC.swift
+++ RxSwiftSample/View/SettingsVC.swift
@@ -16,42 +16,21 @@
 
 import UIKit
 import RxSwift
-import RxDataSources
+import RxCocoa
 
 class SettingsVC: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
 
     private var disposeBag = DisposeBag()
-
-    // numberOfSections,tableReloadの役割
-    private lazy var dataSource = RxTableViewSectionedReloadDataSource<SettingsSectionModel>(configureCell: configureCell)
-
-    // cellForRowAtの役割ーセルを返す
-    private lazy var configureCell: RxTableViewSectionedReloadDataSource<SettingsSectionModel>.ConfigureCell =
-        { [weak self]  (dataSource, tableView, indexPath, _) in
-            let item = dataSource[indexPath]
-            switch item {
-            case .account, .security, .notification, .contents, .sounds, .dataUsing, .accessibility, .one, .two:
-                let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-                cell.textLabel?.text = item.title
-                return cell
-            case .description(let text):
-                let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-                cell.textLabel?.text = text
-                cell.isUserInteractionEnabled = false
-                return cell
-            }
-        }
-
-    private var viewModel: SettingsViewModel!
+    // タスクを保持
+    private let tasks = BehaviorRelay<[Task]>(value: [])
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupViewController()
         setupTableView()
-        setupViewModel()
     }
 
     // 起動時の設定
@@ -69,71 +48,14 @@ class SettingsVC: UIViewController {
 
     // TableViewの設定
     private func setupTableView() {
-        // delegateやセルの登録
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        tableView.contentInset.bottom = 12.0
-        tableView.rx
-            .setDelegate(self)
-            .disposed(by: disposeBag)
-        // アラートを生成
-        let alert = PublishSubject<Void>()
-        alert
-            .subscribe { (action) in
-                let alert = UIAlertController(title: "アラート", message: "メッセージ", preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-                alert.addAction(UIAlertAction(title: "キャンセル", style: .cancel, handler: nil))
-                self.present(alert, animated: true, completion: nil)
-            }
-            .disposed(by: disposeBag)
         // didTapSelectRowAt-セルをタップ時の処理
         // itemSelected時にsubscribe(タップされたら購読)
         tableView.rx.itemSelected
             .subscribe(onNext: { [weak self] indexPath in
-                guard let item_ = self?.dataSource[indexPath] else {return}
                 self?.tableView.deselectRow(at: indexPath, animated: true)
                 let vc = CreateAndUpdateVC(nibName: "CreateAndUpdateVC", bundle: nil)
                 self?.navigationController?.pushViewController(vc, animated: true)
             })
             .disposed(by: disposeBag)
-    }
-
-    private func setupViewModel() {
-        viewModel = SettingsViewModel()
-        // ViewModelとdataSourceを紐付け？
-        viewModel.items
-            .bind(to: tableView.rx.items(dataSource: dataSource))
-            .disposed(by: disposeBag)
-        viewModel.updateItem()
-    }
-}
-
-extension SettingsVC: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        let item = dataSource[indexPath]
-        return item.rowHeight
-    }
-
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        let section = dataSource[section]
-        return section.model.headerHeight
-    }
-
-    func tableView(_ tableView: UITableView, estimatedHeightForFooterInSection section: Int) -> CGFloat {
-        let section = dataSource[section]
-        return section.model.footerHeight
-    }
-
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let headerView = UITableViewHeaderFooterView()
-        /*
-            dataSource.sectionModels = {
-                [model:account, items:[...]],
-                [model:common, items:[...]],
-                [model:original, items:[...]]
-            }
-         */
-        headerView.textLabel?.text = dataSource.sectionModels[section].model.title
-        headerView.backgroundColor = .lightGray
-        return headerView
     }
 }
